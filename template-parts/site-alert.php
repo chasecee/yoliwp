b3b005975460201644fe714753acb7380bc75da3
Chase Christensen <chaserchristensen@gmail.com>
2021-08-09 11:41:40 -0600

    adding contact info to site-alert.php

--- template-parts/site-alert.php
+++ template-parts/site-alert.php
@@ -13,10 +13,14 @@
 <div class="site-alert">
 	<div class="container">
 		<div class="grid grid-cols-12 h-60 items-center">
-			<div class="text-center col-span-8 col-start-3 ">
+		<div class="col-span-3 flex justify-start">
+				<div class="flex items-center mr-24"><span class="">jen.furness@gmail.com</span> </div>
+				<div class="flex items-center mr-0"><span class="">(801) 837-2451</span> </div>
+			</div>
+			<div class="text-center col-span-6 ">
 				Welcome to Jen Furness’ Experience
 			</div>
-			<div class="col-span-2 flex justify-end">
+			<div class="col-span-3 flex justify-end">
 				<div class="flex items-center ml-24"><span class="mr-5">En</span> <?php get_template_part( '/src/images/icons/inline/inline', 'caret-down.svg' ); ?></div>
 				<div class="flex items-center ml-24"><span class="mr-5">Country</span> <?php get_template_part( '/src/images/icons/inline/inline', 'caret-down.svg' ); ?></div>
 			</div>
