69ab55ab86d11cef78bcd0601b03a820a50b8e0a
Nathan Rhead Cox <nrc4@hotmail.com>
2021-08-25 18:42:56 -0700

    Deleted unnecessary files from the web-alias directory.

--- web-alias/js-components/handle-select.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Receives the value selected in the language and country dropdowns and does something with it; currently, it just console logs it.
-function handleSelect(event) {
-  event.preventDefault();
-  // localStorage.setItem('country-language', [...event]);
-  console.log(event.target.value);
-
-}
--- web-alias/js-components/products.php
+++
@@ -1,6 +0,0 @@
-<?php 
-  echo '<script src="app.js"></script>';
-  echo '<script type="module" src="js-components/repsite-validation.js"></script>';
-  // require 'js-components/repsite-validation.js'
-?>
-  
