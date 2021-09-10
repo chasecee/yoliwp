0f69b95304f8f96ae5fc1217aa1b261a0aa5bb1e
Nathan Rhead Cox <nrc4@hotmail.com>
2021-09-10 10:19:30 -0700

    Gave get-item-id its own php file, separating it from get-url.

---
+++ api/get-item-id.php
@@ -0,0 +1,12 @@
+<?php
+function get_item_id() {
+	$link = ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ?
+	'https' : 'http' ) . '://' . $_SERVER['HTTP_HOST'] .
+	$_SERVER['REQUEST_URI'];
+
+	$link_components = parse_url( $link );
+	parse_str( $link_components['query'], $params);
+
+	return $params['item_id'];
+}
+?>
--- api/get-url.php
+++ api/get-url.php
@@ -33,15 +33,4 @@ function get_url() {
 
 	web_alias( $path, $home );
 }
-
-function get_item_id() {
-	$link = ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ?
-	'https' : 'http' ) . '://' . $_SERVER['HTTP_HOST'] .
-	$_SERVER['REQUEST_URI'];
-
-	$link_components = parse_url( $link );
-	parse_str( $link_components['query'], $params);
-
-	return $params['item_id'];
-}
 ?>
