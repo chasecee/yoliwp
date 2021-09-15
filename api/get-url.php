fa44f3078d0a4a425024d74b5f81a0410951b77b
Nathan Rhead Cox <nrc4@hotmail.com>
2021-09-15 10:55:56 -0700

    Clean up get-url's array of extant pages.

--- api/get-url.php
+++ api/get-url.php
@@ -19,7 +19,7 @@ function get_url() {
 
 	// Will return an array of all valid, client-facing wp-pages, e.g., ( [0] => earn [1] => home [2] => our-story [3] => product-data [4] => products [5] => alkalete [6] => cheers [7] => defend [8] => passion [9] => shine [10] => yes [11] => sample-page [12] => scaffolding ).
 	$wp_pages = array_column( get_pages(), 'post_name' );
-	$wp2 = array('wp-admin', 'robots.txt', 'sitemap.xml', 'wp-login.php', 'admin-ajax.php', 'admin-post.php', 'post.php', 'index.php');
+	$wp2 = array('robots.txt', 'sitemap.xml', 'admin-ajax.php', 'admin-post.php', 'post.php', 'index.php');
 
 	$real_paths = array_merge($wp_pages, $wp2);
 
@@ -31,9 +31,6 @@ function get_url() {
 			'/products/' . $page === $path ||
 			'/archives/' . $page === $path ||
 			strpos($link, '/wp-') !== false
-
-			// strpos($link, '/wp-admin/') !== false ||
-			// strpos($link, '/wp-json/') !== false
 			) :
 			$redirect = '/';
 		endif;
